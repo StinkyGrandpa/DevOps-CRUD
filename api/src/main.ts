ebd5214c713b69a55362641a069b437c2a4e40ae
Robert <92435066+StinkyGrandpa@users.noreply.github.com>
2023-03-21 11:40:20 +0100

    enable Cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,14 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors({ origin: ['*', 'http://localhost:4200'] })
+  app.useGlobalPipes(new ValidationPipe({
+    //disableErrorMessages: true,
+    transform: true,
+  }));
 }
 bootstrap();
