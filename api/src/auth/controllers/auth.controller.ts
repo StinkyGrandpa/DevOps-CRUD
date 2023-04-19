7cc2ede992d67a5a9f6ebb5b37a6fccd4c2cae2c
Robert <92435066+StinkyGrandpa@users.noreply.github.com>
2023-04-19 08:41:13 +0200

    updated swagger

--- api/src/auth/controllers/auth.controller.ts
+++ api/src/auth/controllers/auth.controller.ts
@@ -3,17 +3,20 @@ import { AuthService } from '../auth.service';
 import { CreateAuthDto } from '../dto/create-auth.dto';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { LocalAuthGuard } from '../guards/local-auth.guard';
+import { ApiBody } from '@nestjs/swagger';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @ApiBody({ description: 'Login Routine' })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   login(@Body() createAuthDto: CreateAuthDto) {
     return this.authService.login(createAuthDto);
   }
 
+  @ApiBody({ description: 'Validiert den Token' })
   @UseGuards(JwtAuthGuard)
   @Get('validate')
   validate() {
--- api/src/auth/dto/create-auth.dto.ts
+++ api/src/auth/dto/create-auth.dto.ts
@@ -1,10 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsString } from "class-validator";
 
 export class CreateAuthDto {
-
+    @ApiProperty({ nullable: false, description: 'Benutzername' })
     @IsString()
     username: string
 
+    @ApiProperty({ nullable: false, description: 'Passwort' })
     @IsString()
     password: string
 }
